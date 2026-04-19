78f393b50bc361c4da5047fa098d1f3781535112
Pim van Leeuwen <leeuwen.pimvan@pm.me>
2026-04-19 09:39:04 +0000

    Rename integration blueprint to savs

--- custom_components/integration_blueprint/coordinator.py
+++ custom_components/integration_blueprint/coordinator.py
@@ -1,4 +1,4 @@
-"""DataUpdateCoordinator for integration_blueprint."""
+"""DataUpdateCoordinator for savs."""
 
 from __future__ import annotations
 
--- custom_components/integration_blueprint/switch.py
+++ custom_components/integration_blueprint/switch.py
@@ -1,4 +1,4 @@
-"""Switch platform for integration_blueprint."""
+"""Switch platform for savs."""
 
 from __future__ import annotations
 
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
 
 ENTITY_DESCRIPTIONS = (
     SwitchEntityDescription(
-        key="integration_blueprint",
+        key="savs",
         name="Integration Switch",
         icon="mdi:format-quote-close",
     ),
@@ -40,7 +40,7 @@ async def async_setup_entry(
 
 
 class IntegrationBlueprintSwitch(IntegrationBlueprintEntity, SwitchEntity):
-    """integration_blueprint switch class."""
+    """savs switch class."""
 
     def __init__(
         self,
